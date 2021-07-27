953fdad5a1dd713f6fd9570dde2a986cfb944090
bugbuilder <nelson@glacierdao.io>
2021-07-26 22:10:11 -0400

    fix: adding LendingRateOracle to mock deployment

--- tasks/dev/7_glacier_oracles.ts
+++ tasks/dev/7_glacier_oracles.ts
@@ -2,19 +2,28 @@ import { utils } from 'ethers';
 import { isAddress } from 'ethers/lib/utils';
 import { task } from 'hardhat/config';
 
-import { ConfigNames, getWethAddress, loadPoolConfig } from '../../helpers/configuration';
+import {
+  ConfigNames,
+  getWethAddress,
+  loadPoolConfig,
+  getLendingRateOracles,
+  getGenesisPoolAdmin,
+} from '../../helpers/configuration';
 import { ZERO_ADDRESS } from '../../helpers/constants';
-import { deployGlacierOracle } from '../../helpers/contracts-deployments';
+import { deployGlacierOracle, deployLendingRateOracle } from '../../helpers/contracts-deployments';
 import {
   getAaveOracle,
   getLendingPoolAddressesProvider,
   getPairsTokenAggregator,
+  getLendingRateOracle,
 } from '../../helpers/contracts-getters';
 import { getParamPerNetwork } from '../../helpers/contracts-helpers';
-import { waitForTx } from '../../helpers/misc-utils';
+import { waitForTx, notFalsyOrZeroAddress } from '../../helpers/misc-utils';
 import { getAllAggregatorsAddresses } from '../../helpers/mock-helpers';
 import { deployAllMockAggregators } from '../../helpers/oracles-helpers';
 import { eNetwork, ICommonConfiguration, SymbolMap } from '../../helpers/types';
+import { LendingRateOracle } from '../../types';
+import { setInitialMarketRatesInRatesOracleByHelper } from '../../helpers/oracles-helpers';
 
 task('eth', 'Deploy oracles for dev enviroment')
   .addParam('oracle', 'Oracle')
@@ -36,6 +45,8 @@ task('dev:deploy-glacier-oracle', 'Deploy oracles for dev enviroment')
     await DRE.run('set-DRE');
     const network = <eNetwork>DRE.network.name;
     const poolConfig = loadPoolConfig(pool);
+    const lendingRateOracles = getLendingRateOracles(poolConfig);
+    const admin = await getGenesisPoolAdmin(poolConfig);
     const {
       Mocks: { AllAssetsInitialPrices },
       ReserveAssets,
@@ -43,7 +54,7 @@ task('dev:deploy-glacier-oracle', 'Deploy oracles for dev enviroment')
     } = poolConfig as ICommonConfiguration;
 
     const reserveAssets = getParamPerNetwork(ReserveAssets, network);
-
+    const lendingRateOracleAddress = getParamPerNetwork(poolConfig.LendingRateOracle, network);
 
     Object.keys(AllAssetsInitialPrices).forEach(function (key) {
       if (key != 'gCLP' && key != 'USD') {
@@ -72,9 +83,24 @@ task('dev:deploy-glacier-oracle', 'Deploy oracles for dev enviroment')
       verify
     );
 
+    let lendingRateOracle: LendingRateOracle;
+    if (notFalsyOrZeroAddress(lendingRateOracleAddress)) {
+      lendingRateOracle = await getLendingRateOracle(lendingRateOracleAddress);
+    } else {
+      lendingRateOracle = await deployLendingRateOracle(verify);
+      const { USD, ...tokensAddressesWithoutUsd } = tokensToWatch;
+      await setInitialMarketRatesInRatesOracleByHelper(
+        lendingRateOracles,
+        tokensAddressesWithoutUsd,
+        lendingRateOracle,
+        admin
+      );
+    }
+
     if (isAddress(addressesprovider)) {
       const addressesProvider = await getLendingPoolAddressesProvider(addressesprovider);
       await waitForTx(await addressesProvider.setPriceOracle(glacierOracle.address));
+      await waitForTx(await addressesProvider.setLendingRateOracle(lendingRateOracle.address));
     }
 
   });
